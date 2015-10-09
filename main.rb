3db4c00e69535f1c43d8688a537c9857d7fec6f7
Alex <a.b320012@gmail.com>
2015-10-09 11:57:27 -0400

    email functionality

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'mandrill'
 
 get '/' do 
 	@title= "Home"
@@ -24,7 +25,23 @@ get '/form' do
 	erb :form
 end
 
-post '/mail' do
-	@title = "Sent Form"
-	rb :mail
+ post '/mail' do
+ 	api_key = ENV['MANDRILL_APIKEY']
+    m = Mandrill::API.new "#{api_key}"
+    message = {
+        :subject => "Hello from the Mandrill API",
+        :from_name => "#{params[:name]} #{params[:email]}",
+        :text => "Hi message how are you?",
+        :to => [
+            {
+                :email => "a.b320012@gmail.com",
+                :name => "Recipient1"
+             }
+        ],
+        :html => "<html>#{params[:msg]}</html>",
+        :from_email => "anonymous@gmail.com"
+    }
+    sending = m.messages.send message
+    puts sending
+    erb :home
 end
